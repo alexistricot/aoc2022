e85412ceda50b79907740591863fc6e9d4eb49c1
Alexis Tricot <al.tricot1@gmail.com>
2022-12-08 00:31:30 +0100

    Improve day 7 syntax

--- day7.rb
+++ day7.rb
@@ -1,23 +1,22 @@
 # Day 7 of the advent of code 2022
 
+# implements a file
 class File
-    attr_reader :name
-    attr_reader :size
+    attr_reader :name, :size
 
     def initialize(name, size)
-        @name=name
-        @size=size.to_i
+        @name = name
+        @size = size.to_i
     end
 
     def to_s(indent)
-        "  " * indent + "- #{@name} (file, size=#{@size})\n"
+        '  ' * indent + "- #{@name} (file, size=#{@size})\n"
     end
 end
 
+# implements a directory
 class Directory
-    attr_reader :name
-    attr_reader :parent
-    attr_reader :size
+    attr_reader :name, :parent
     attr_accessor :children
 
     def initialize(name, parent)
@@ -30,83 +29,73 @@ class Directory
     end
 
     def process_list(str)
-        if children
-            # we already listed the contents
-            return
-        end
-        @children = Hash.new()
+        return if children
+
+        @children = {}
         str.split("\n").each do |line|
             case line
-            when /^dir .+$/ then
-                name = /^dir (.+)$/.match(line).captures.first
+            when /^dir (.+)$/
+                name = Regexp.last_match(1)
                 @children[name] = Directory.new(name, self)
-            when /^\d+ [\w\.]+$/ then
-                size, name = /^(\d+) ([\w\.]+)$/.match(line).captures
+            when /^(\d+) ([\w.]+)$/
+                size = Regexp.last_match(1)
+                name = Regexp.last_match(2)
                 @children[name] = File.new(name, size)
             end
         end
     end
 
-    def size()
-        if not @size
-            @size = @children.each_value.map(&:size).sum
-        end
-        return @size
+    def size
+        @size ||= @children.each_value.map(&:size).sum
+        @size
     end
 
-    def to_s(indent=0)
-        "  " * indent + "- #{@name} (dir, size=#{self.size})\n" + @children.each_value.map { |elem| elem.to_s(indent + 1)}.join("")
+    def to_s(indent = 0)
+        desc = '  ' * indent + "- #{@name} (dir, size=#{size})\n"
+        desc + @children.each_value.map { |elem| elem.to_s(indent + 1) }.join('')
     end
 end
 
 def directory_from_file(path)
-    fs = Directory.new("/", nil)
+    fs = Directory.new('/', nil)
     current = fs
-
-    File.read(path).split("$ ").each do |command|
+    File.read(path).split('$ ').each do |command|
         case command
         when /^cd \.\./
             current = current.parent
-        when /^cd \w+$/
-            name = /^cd ([\w]+)$/.match(command).captures.first
-            current = current.children[name]
+        when /^cd (\w+)$/
+            current = current.children[Regexp.last_match(1)]
         when /^ls/
             current.process_list(command)
         end
     end
-    return fs
+    fs
 end
 
+def sum_small_dir_sizes(fs, limit = 100_000)
+    return 0 if fs.instance_of?(File)
 
-def sum_small_dir_sizes(fs, limit=100000)
-    if fs.class == File
-        return 0
-    end
     # fs is a Directory
     own_size = fs.size <= limit ? fs.size : 0
-    return own_size + fs.children.each_value.map { |dir| sum_small_dir_sizes(dir) }.sum
+    own_size + fs.children.each_value.map { |dir| sum_small_dir_sizes(dir) }.sum
 end
 
-fs = directory_from_file("./inputs/day7.txt")
+fs = directory_from_file('./inputs/day7.txt')
 
 puts sum_small_dir_sizes(fs)
 
-max_size = 70000000
-necessary = 30000000
+max_size = 70_000_000
+necessary = 30_000_000
 
 min_to_delete = fs.size - (max_size - necessary)
 
 puts min_to_delete
 
 def min_max_dir_size(fs, limit)
-    if fs.class == File
-        return 1.0 / 0.0
-    end
-    if fs.size >= limit
-        return [fs.size, fs.children.each_value.map { |dir| min_max_dir_size(dir, limit) }.min].min
-    else
-        return 1.0 / 0.0
-    end
+    return Float::INFINITY if fs.instance_of?(File)
+    return [fs.size, fs.children.each_value.map { |dir| min_max_dir_size(dir, limit) }.min].min if fs.size >= limit
+
+    Float::INFINITY
 end
 
 puts min_max_dir_size(fs, min_to_delete)
